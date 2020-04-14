b1697ecfd740e2cb294942d9e6e83752eb450637
Anne Bailey <anne.g.bailey.22@dartmouth.edu>
2020-04-14 11:51:22 -0400

    initial starterpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 /* eslint-disable semi */
-console.log('starting up');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+}, 1000);
+console.log(num);
