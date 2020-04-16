9bd8d499253a6d135d0313a6395ea24d31349fe3
Anne Bailey <anne.g.bailey.22@dartmouth.edu>
2020-04-16 11:07:47 -0400

    added React support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-/* eslint-disable semi */
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let num = 0;
-setInterval(() => {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds.`);
-}, 1000);
-console.log(num);
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
