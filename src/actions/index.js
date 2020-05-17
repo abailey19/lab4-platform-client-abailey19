5514535078019f43bd826e513395b0a686314545
Anne Bailey <anne.g.bailey.22@dartmouth.edu>
2020-05-16 21:15:28 -0400

    changed root url to heroku server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const ROOT_URL = 'http://abailey-blog.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'http://abailey-blog.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
 const API_KEY = '?key=a_bailey';
 
 export const ActionTypes = {
